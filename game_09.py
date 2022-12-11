891e1d8a09f0709b70d9e0739fe3efdb7a700796
WHY2B <whyso2bx@gmail.com>
2022-12-11 20:52:33 +0700

    update bg, border-bg

--- game_09.py
+++ game_09.py
@@ -8,14 +8,12 @@ import sys
 import os
 from PIL import Image, ImageTk
 
-pygame.mixer.init()
 
 customtkinter.set_appearance_mode("Light")
 customtkinter.set_default_color_theme("blue")
 
-
-
 #instrumentals
+pygame.mixer.init()
 bg_music = pygame.mixer.music.load('bg.wav')
 correct_sound = pygame.mixer.Sound('correct.wav')
 wrong_sound = pygame.mixer.Sound('buzzer.wav')
@@ -38,12 +36,14 @@ icon = PhotoImage(file = 'icon.jpg')
 window.iconphoto(False, icon)
 
 #bg
-# window.config(bg = '#add123')
-# image = Image.open("test_bg_01.png")
-# photo = ImageTk.PhotoImage(image)
-# window.wm_attributes('-transparentcolor','#add123')
-# label1 = Label( window, image = photo)
-# label1.place(x = 0, y = 0)
+window.config(bg = '#add123')
+image = Image.open("test_bg_01.png")
+photo = ImageTk.PhotoImage(image)
+window.wm_attributes('-transparentcolor','#add123')
+label1 = Label( window, image = photo)
+label1.place(x = 0, y = 0)
+
+
 
 #สถานะเกม
 game_end = False #เริ่มต้นเกมยังไม่จบ
@@ -59,7 +59,7 @@ status_str = StringVar() #มาจาก Tkinter
 # status_str.pack(pady=10, padx=150)
 
 status_str.set('Score: '+ str(score) + ' | ' + 'Lives: ' + '♥'*lives)
-show_status = Label(window, textvariable=status_str, font=('FC Minimal', 20))
+show_status = Label(window, textvariable=status_str, font=('FC Minimal', 20), bg = '#f4d575')
 show_status.pack(pady=20)
 
 #สร้าง words category hints
@@ -199,12 +199,12 @@ secret_word, clue = new_secret_word()
 #แสดง category, clue บน window
 category_str = StringVar()
 category_str.set(word_dict[secret_word]['category'])
-show_category = Label(window, textvariable=category_str, font=('FC Minimal', 20)) 
+show_category = Label(window, textvariable=category_str, font=('FC Minimal', 20), bg = '#f4d575') 
 show_category.pack(pady=10)
 
 clue_str = StringVar()
 clue_str.set(' | '.join(clue))
-show_clue = Label(window, textvariable=clue_str, font=('FC Minimal', 35))
+show_clue = Label(window, textvariable=clue_str, font=('FC Minimal', 35), bg = '#f4d575')
 show_clue.pack(padx=10, pady=10)
 
 #เเสดง hints
@@ -214,9 +214,10 @@ hints_text.set('hints')
 hints_str = StringVar()
 hints_str.set('\n'.join(hints))
 
-show_hint_text = Label(window, textvariable=hints_text, font=('FC Minimal', 20))
+
+show_hint_text = Label(window, textvariable=hints_text, font=('FC Minimal', 20), bg = '#f4d575')
 show_hint_text.pack()
-show_hint_text = Label(window, textvariable=hints_str, font=('FC Minimal', 28))
+show_hint_text = Label(window, textvariable=hints_str, font=('FC Minimal', 28), bg = '#f4d575')
 show_hint_text.pack(pady=10)
 
 #ปุ่มกด submit พร้อมฟังก์ชันที่อัพเดต clue และสถานะของเกม
@@ -230,11 +231,15 @@ def update_clue(guess, secret_word, clue):
     win = ''.join(clue) == secret_word
     return win
 
+#frame - เอาไว้เติมสีของ CTK txtentry
+frame1 = Frame( window, bg = "#f4d575", border=2)
+frame1.pack(pady = 1)
+
 #กล่องให้กรอก guess 
-textentry = customtkinter.CTkEntry(master=window ,width=200,
+textentry = customtkinter.CTkEntry(master=frame1 ,width=200,
                 font=customtkinter.CTkFont("FC Minimal", size=20), justify='center', corner_radius=15)
 
-def update_screen(event):
+def update_screen(event=None):
     #ประกาศตัวแปรพวกนี้ให้เป็น global เพื่อให้ฟังก์ชันนี้เข้าถึงตัวแปรใน command ได้
     global game_end, score, lives, secret_word, clue, hints
 
@@ -254,7 +259,6 @@ def update_screen(event):
             time.sleep(1) #ทำให้โปรแกรมค้างไว้ 1 วิ
             status_str.set('Score : ' + str(score) + ' | ' + 'Lives: ' + '♥'*lives)
             textentry.delete(0, 'end')
-            
             
             if len(words) < 1:
                 game_end = True
@@ -278,58 +282,21 @@ def update_screen(event):
         status_str.set('Score : ' + str(score) + ' | ' + 'Lives: ' + '♥'*lives)
         textentry.delete(0, 'end')
 
-def update_screen_2():
-    #ประกาศตัวแปรพวกนี้ให้เป็น global เพื่อให้ฟังก์ชันนี้เข้าถึงตัวแปรใน command ได้
-    global game_end, score, lives, secret_word, clue, hints
-
-    guess = textentry.get().strip() #Strip to remove whitespaces
-    guess = guess.lower() #lowercase
-
-    if guess in secret_word:
-        
-        win = update_clue(guess, secret_word, clue)
-        if win:
-            #print('เฉลยตึงๆ : ' + secret_word)
-            score += 1
-            print('Score : ' + str(score))
-            correct_sound.play()
-            clue_str.set('Ahe! Ans : ' + secret_word)
-            window.update()
-            time.sleep(1) #ทำให้โปรแกรมค้างไว้ 1 วิ
-            status_str.set('Score : ' + str(score) + ' | ' + 'Lives: ' + '♥'*lives)
-            textentry.delete(0, 'end')
-            
-            if len(words) < 1:
-                game_end = True
-                clue_str.set('Congrats!')
-            else:
-                secret_word, clue = new_secret_word()
-                category_str.set(word_dict[secret_word]['category'])
-                clue_str.set(' | '.join(clue))
-                hints = word_dict[secret_word]['hints']
-                hints_str.set('\n'.join(hints))
-    else:
-        print('ผิดแล้ว ไอควาย หน้าโง่')
-        lives -= 1
-        wrong_sound.play()
-        textentry.delete(0, 'end')
-        if lives < 1:
-            clue_str.set('Game Over!')
-            game_end = True
-
-        status_str.set('Score : ' + str(score) + ' | ' + 'Lives: ' + '♥'*lives)
-        textentry.delete(0, 'end')
-
 textentry.bind('<Return>', update_screen)
 textentry.pack(pady=10, padx=225)
-    
-button = customtkinter.CTkButton(master=window, text="Submit",
+
+#frame - เอาไว้เติมสีของ CTK button
+frame2 = Frame( window, bg = "#f4d575", border=2, borderwidth=1)
+frame2.pack(pady = 0)
+
+button = customtkinter.CTkButton(master=frame2, text="Submit",
                                 bg_color = 'transparent',
-                                border_width=2,  # <- custom border_width
+                                border_width=1,  # <- custom border_width
                                 border_spacing=0,
-                                fg_color=None,  # <- no fg_color
+                                fg_color= 'white',  # <- no fg_color
                                 corner_radius=10,
-                                command=update_screen_2)
+                                text_color = 'black',
+                                command=update_screen)
 button.pack(pady=20, padx=225)
 
 #โปรแกรมหลักที่ check ว่าจบเกมแล้วหรือยัง
@@ -344,6 +311,7 @@ def Close():
     window.destroy()
 
 def main():
+    
     if not game_end:
         #print('Test Refresh')
         window.after(1000, main)
@@ -372,3 +340,5 @@ window.mainloop()
 
 #จบเกม
 print('End Game')
+
+
